aa605129002daa846ba56c3caee603921b81e490
Kevin Grandjean <moi@kevingrandjean.ch>
2024-06-26 19:32:31 +0200

    Add gauge test

--- src/internal/interfaces/prometheus/handler_test.go
+++ src/internal/interfaces/prometheus/handler_test.go
@@ -71,3 +71,49 @@ func Test_published_counters_are_present_on_page(t *testing.T) {
 		t.Fatalf("expects gitfortress metrics at the moment. Got \n%v", string(body))
 	}
 }
+
+func Test_published_gauges_are_present_on_page(t *testing.T) {
+	prometheusHandler := NewPrometheusMetricsHandler(
+		MetricsHandlerOpts{
+			ExposedPort:      8080,
+			AutoConvertNames: false,
+			MetricPrefix:     "gitfortress",
+		},
+	)
+
+	ctx := context.Background()
+	go prometheusHandler.Start(ctx)
+	defer ctx.Done()
+
+	body := getMetricsBody(t)
+
+	if strings.Contains(string(body), "gitfortress_") {
+		t.Fatalf("expects no gitfortress metrics at the moment. Got \n%v", string(body))
+	}
+
+	fakeMetricsService := fakeMetricsService{metricsPort: prometheusHandler}
+	gauge := entity.NewGauge("some_gauge", &fakeMetricsService)
+
+	gauge.SetInt("some_value", 0)
+
+	body = getMetricsBody(t)
+
+	if !strings.Contains(string(body), "gitfortress_some_gauge_some_value 0") {
+		t.Fatalf("expects gitfortress metrics at the moment. Got \n%v", string(body))
+	}
+
+	gauge.SetFloat("some_value", 1.2)
+	gauge.SetInts(map[string]int{"some_other_value": 1, "some_different_value": 3})
+
+	body = getMetricsBody(t)
+
+	if !strings.Contains(string(body), "gitfortress_some_gauge_some_value 1.2") {
+		t.Fatalf("expects gitfortress metrics at the moment. Got \n%v", string(body))
+	}
+	if !strings.Contains(string(body), "gitfortress_some_gauge_some_other_value 1") {
+		t.Fatalf("expects gitfortress metrics at the moment. Got \n%v", string(body))
+	}
+	if !strings.Contains(string(body), "gitfortress_some_gauge_some_different_value 3") {
+		t.Fatalf("expects gitfortress metrics at the moment. Got \n%v", string(body))
+	}
+}
